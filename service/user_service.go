f0b4dfbb2ae15d4dec4e0878f445cbad059176c4
chaeyoungeee <chaeinge@gmail.com>
2025-11-17 00:28:46 +0900

    refactor: 에러 출력 형식 변경

--- service/user_service.go
+++ service/user_service.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"errors"
-	"fmt"
 
 	"github.com/chaeyoungeee/blog-feed-notifier/domain"
 	"github.com/chaeyoungeee/blog-feed-notifier/repository"
@@ -17,19 +16,12 @@ func NewUserService(repo *repository.UserRepo) *UserService {
 }
 
 func (s *UserService) CreateUser(user *domain.User) error {
-	if user.Username == "" {
-		return fmt.Errorf("유저 이름은 비어 있을 수 없습니다")
-	}
-	if user.Password == "" {
-		return fmt.Errorf("비밀번호는 비어 있을 수 없습니다")
-	}
-
 	exists, err := s.Repo.ExistsByUsername(user.Username)
 	if err != nil {
 		return err
 	}
 	if exists {
-		return fmt.Errorf("이미 존재하는 유저 이름입니다")
+		return errors.New("이미 존재하는 유저 이름입니다")
 	}
 
 	return s.Repo.Create(user)
