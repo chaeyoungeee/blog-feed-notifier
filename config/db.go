91841fad2a56038b036b7748f272a9fe8dc918e3
chaeyoungeee <chaeinge@gmail.com>
2025-11-17 23:01:54 +0900

    refactor: 환경변수 설정

--- config/db.go
+++ config/db.go
@@ -2,13 +2,20 @@ package config
 
 import (
 	"fmt"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func InitDB() (*gorm.DB, error) {
-	dsn := "user:pw@tcp(localhost:3309)/blog-feed-notifier-db?parseTime=true"
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	name := os.Getenv("DB_NAME")
+
+	dsn := user + ":" + pass + "@tcp(" + host + ":" + port + ")/" + name + "?parseTime=true"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect database: %w", err)
