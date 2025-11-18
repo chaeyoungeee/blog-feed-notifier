f9812a4c3758df46364ccb426f50275bc130bc7e
chaeyoungeee <chaeinge@gmail.com>
2025-11-18 10:42:55 +0900

    fix: 구독 중복 저장 버그 해결

--- domain/subscription.go
+++ domain/subscription.go
@@ -4,8 +4,8 @@ import "time"
 
 type Subscription struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
-	UserID    uint      `gorm:"not null" json:"user_id"`
-	BlogID    uint      `gorm:"not null" json:"blog_id"`
+	UserID    uint      `gorm:"not null;uniqueIndex:idx_user_blog" json:"user_id"`
+	BlogID    uint      `gorm:"not null;uniqueIndex:idx_user_blog" json:"blog_id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 
